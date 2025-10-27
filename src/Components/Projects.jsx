43ac8572e6851137568b43d78e70fa18d9b40925
SUNEEL KUMAR <122699505+formaxx786@users.noreply.github.com>
2025-10-27 13:52:38 +0530

    Fix duplicate Projects header in Projects component

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,6 +6,10 @@ function Projects() {
     <motion.h2 whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-80}} transition={{duration:1.2}} className='my-20 text-center text-4xl'>Projects</motion.h2>
   
 </div>
+    <div className=' border-b border-neutral-900 pb-4'>
+    <motion.h2 whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-80}} transition={{duration:1.2}} className='my-20 text-center text-4xl'>Projects</motion.h2>
+  
+</div>
   )
 }
 
